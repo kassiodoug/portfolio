a80c122656b256a0359274289d68668501010610
Kassio Douglas <kassio22douglas@gmail.com>
2026-04-09 18:07:09 -0300

    Adição do Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header.tsx";
 import Hero from "./components/Hero.tsx";
 import Projects from "./components/Projects";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Header />
       <main>
         <Hero />
