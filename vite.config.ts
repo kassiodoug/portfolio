a012b2cf18d91aaa44815f01565e9cfa485c4447
Kassio Douglas <kassio22douglas@gmail.com>
2026-04-09 17:04:35 -0300

    Remoção da propriedade base do vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_URL || "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
